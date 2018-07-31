eb7874f0fd4c7c2c9078aa8fc98d35901de7a4d7
Steph Milovic <steph.milovic@craftsy.com>
2018-07-30 18:36:57 -0600

    thought things were working, getting sass errs

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -16,7 +16,7 @@ export default props => {
     return (
         <div className="card">
             <div className="card-image">
-                <img src={featuredImage} alt={name} />
+                <img src={require(`src/${featuredImage}`)} alt={name} />
             </div>
             <div className="card-header">
                 <h3>{name}</h3>
@@ -25,18 +25,12 @@ export default props => {
                 <p>{summary}</p>
                 <LinkModal {...props} />
                 <Tags tags={tags} />
-                <p display-if={privateMsg} className="cb">
-                    {privateMsg}
-                </p>
+                <p display-if={privateMsg} className="cb">{privateMsg}</p>
                 <p display-if={siteLink && siteGithub}>
                     <span>
-                        <a href={siteLink} target="_blank">
-              Live Site
-                        </a>{' '}
-            -{' '}
-                        <a href={siteGithub} target="_blank">
-              GitHub
-                        </a>
+                        <a href={siteLink} target="_blank">Live Site</a>
+                        {' '}-{' '}
+                        <a href={siteGithub} target="_blank">GitHub</a>
                     </span>
                 </p>
             </div>
